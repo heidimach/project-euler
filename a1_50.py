a080097d33f84bbb9c25c2cbd82f95557bc3a194
heidimach <heidi_96@outlook.com>
2021-03-04 20:33:25 +0100

    Task 2
    Fibonacci numbers

--- a1_50.py
+++ a1_50.py
@@ -10,3 +10,23 @@ def multiples_sum(B=10,a=3,b=5):
     L=[i for i in range(1,B) if (i%a==0 or i%b==0)]
     return sum(L)
 
+def fibonacci_sum(B,value=None):
+    prev=[1,2]
+    if value=="Even":
+        L=prev[1]
+        while sum(prev)<B:
+            prev=[prev[1],sum(prev)]
+            if (prev[1])%2==0:
+                L=L+prev[1]
+    elif value=="Odd":
+        L=prev[0]
+        while sum(prev)<B:
+            prev=[prev[1],sum(prev)]
+            if (prev[1])%2==1:
+                L=L+prev[1]
+    else:
+        L=sum(prev)
+        while sum(prev)<B:
+            prev=[prev[1],sum(prev)]
+            L=L+prev[1]
+    return L
--- pyt-test.py
+++ pyt-test.py
@@ -8,3 +8,6 @@ import a1_50 as a
 
 ## Task 1: Multiple of 3 and 5
 print(a.multiples_sum(1000))
+
+## Task 2: Even Fibonacci numbers
+print(a.fibonacci_sum(4*1e6,"Even"))
