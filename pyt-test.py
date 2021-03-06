ee2e3f7106d0db6bc4fd46ff21cca4b0eedd9b0c
unknown <heidi_96@outlook.com>
2021-03-06 18:43:29 +0100

    task-8

--- pyt-test.py
+++ pyt-test.py
@@ -6,23 +6,26 @@ Created on Wed Mar  3 23:35:03 2021
 """
 import a1_50 as a
 
-## Task 1: Multiple of 3 and 5
+## Task 1: Multiple of 3 and 5 - 233168
 print(a.multiples_sum(1000))
 
-## Task 2: Even Fibonacci numbers
+## Task 2: Even Fibonacci numbers - 4613732
 print(a.fibonacci_sum_even(4*1e6))
 
-## Task 3: Largest Prime Factor
+## Task 3: Largest Prime Factor - 6857
 print(a.primefactor_max(600851475143))
 
-## Task 4: Palindrome
+## Task 4: Palindrome - 906609
 print(a.palindrome_prod_max(3))
 
-## Task 5: Smallest multiple
+## Task 5: Smallest multiple - 232792560
 print(a.smallest_multiple(20))
 
-## Task 6: Sum square difference
+## Task 6: Sum square difference - 25164150
 print(a.sum_square_diff(100))
 
-## Task 7: nth prime
+## Task 7: nth prime - 104743
 print(a.nth_prime_number(10001))
+
+## Task 8: Largest product in a series - 23514624000
+print(a.largest_product_in_series(13))
